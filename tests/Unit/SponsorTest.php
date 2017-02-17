740559554e5dcb380d30011c6a9d74e45c5fffe8
Carlos Pérez <cperez@MacBook-Pro-de-Carlos.local>
2017-02-17 21:49:43 +0100

    Created test cases

---
+++ tests/Unit/SponsorTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+use App\Sponsor;
+
+class SponsorTest extends TestCase
+{
+    /**
+     * Checks isJuniorSponsor
+     *
+     * @return void
+     */
+    public function testIsJuniorSponsor()
+    {
+        $sponsor = Sponsor::where('name', 'Samsung')->first();
+        $this->assertTrue($sponsor->isJuniorSponsor());
+
+        $sponsor = Sponsor::where('name', 'Toshiba')->first();
+        $this->assertFalse($sponsor->isJuniorSponsor());
+
+        $sponsor = Sponsor::where('name', 'Asus')->first();
+        $this->assertTrue($sponsor->isJuniorSponsor());
+
+        $sponsor = Sponsor::where('name', 'Intel')->first();
+        $this->assertFalse($sponsor->isJuniorSponsor());
+
+        $sponsor = Sponsor::where('name', 'AMD')->first();
+        $this->assertFalse($sponsor->isJuniorSponsor());
+    }
+
+    private function sponsor_in_array($arr, $sponsor) {
+        $found = false;
+
+        foreach ($arr as $obj) {
+            if ($obj->name == $sponsor) {
+                $found = true;
+                break;
+            }
+        }
+
+        return $found;
+    }
+
+    /**
+     * Checks getJuniorSponsors
+     *
+     * @return void
+     */
+    public function testGetJuniorSponsors()
+    {
+        $sponsors = Sponsor::getJuniorSponsors();
+
+        $this->assertEquals(count($sponsors), 2);
+        $this->assertTrue($this->sponsor_in_array($sponsors, 'Samsung'));
+        $this->assertTrue($this->sponsor_in_array($sponsors, 'Asus'));
+    }
+}
